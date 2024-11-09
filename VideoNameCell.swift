481e1f5d67747ea4acf04329fa231dba308985ed
Srijoni <89831977+itskutush@users.noreply.github.com>
2024-11-09 10:31:02 +0530

    CollectionViewCell:VideoNameCell.swift

---
+++ VideoNameCell.swift
@@ -0,0 +1,13 @@
+//
+//  VideoNameCell.swift
+//  videoLesson
+//
+//  Created by user@90 on 09/11/24.
+//
+
+import UIKit
+
+class VideoNameCell: UICollectionViewCell {
+    
+    @IBOutlet var titleLabel: UILabel!
+}
